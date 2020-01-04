9eca7e291ae48d55e427fe1f3e82c3204572e9fa
Hangbo <zhzhanghangbo@hotmail.com>
2020-01-04 13:59:44 -0500

    Finish Database module

--- app/src/main/java/android/example/todolistapplication/database/TaskDao.java
+++ app/src/main/java/android/example/todolistapplication/database/TaskDao.java
@@ -1,4 +1,25 @@
 package android.example.todolistapplication.database;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
 public interface TaskDao {
+
+    @Query("SELECT * FROM task ORDER BY priority")
+    LiveData<List<Task>> loadTaskList();
+
+    @Insert
+    void insertTask(Task task);
+
+    @Delete
+    void deleteTask(Task task);
+
+    @Query("SELECT * FROM task WHERE id = :id")
+    LiveData<Task> loadTaskById(int id);
 }
