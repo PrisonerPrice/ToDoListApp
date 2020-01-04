9766904a73fe5a31065a81ba138acc1db97d1000
Hangbo <zhzhanghangbo@hotmail.com>
2020-01-04 14:26:40 -0500

    Finish Repository module

--- app/src/main/java/android/example/todolistapplication/repository/AppExecutors.java
+++ app/src/main/java/android/example/todolistapplication/repository/AppExecutors.java
@@ -1,4 +1,57 @@
 package android.example.todolistapplication.repository;
 
+import android.os.Handler;
+import android.os.Looper;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
 public class AppExecutors {
+
+    // For singleton instantiation
+    private static final Object LOCK = new Object();
+    private static AppExecutors appExecutors;
+    private final Executor diskIO;
+    private final Executor mainThread;
+    private final Executor networkIO;
+
+    public AppExecutors(Executor diskIO, Executor mainThread, Executor networkIO) {
+        this.diskIO = diskIO;
+        this.mainThread = mainThread;
+        this.networkIO = networkIO;
+    }
+
+    public static AppExecutors getInstance(){
+        if (appExecutors == null){
+            synchronized (LOCK){
+                appExecutors = new AppExecutors(
+                        Executors.newSingleThreadExecutor(),
+                        Executors.newFixedThreadPool(3),
+                        new MainThreadExecutor());
+            }
+        }
+        return appExecutors;
+    }
+
+    public Executor getDiskIO() {
+        return diskIO;
+    }
+
+    public Executor getMainThread() {
+        return mainThread;
+    }
+
+    public Executor getNetworkIO() {
+        return networkIO;
+    }
+
+    private static class MainThreadExecutor implements Executor{
+
+        private Handler mainThreadHandler = new Handler(Looper.getMainLooper());
+
+        @Override
+        public void execute(Runnable command) {
+            mainThreadHandler.post(command);
+        }
+    }
 }
